21138cbbe9bf8233b54e965cc4b43216a19260a2
Danila <da_buddha@bk.ru>
2022-04-14 01:07:02 +0300

    Доделал до раздела квартиры с чистой отделкой

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,26 @@
-// const swiper = new Swiper('.swiper', {
- 
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
+const swiper = new Swiper('.swiper', {
+    effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        allowTouchMove: false,
+        autoplay: {
+            delay: 6000,
+            disableOnInteraction: false,        
+          },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    loop: 'true',
   
-//   });
+  });
+
+
+  const SwiperSecond = new Swiper('.swiper-second', {
+        
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    spaceBetween: 100,
+  });
