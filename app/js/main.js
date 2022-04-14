45eec7871c0be92dcd4f0e2f48c4f23d1f5d192a
Danila <da_buddha@bk.ru>
2022-04-14 14:32:59 +0300

    остался футер

--- app/js/main.js
+++ app/js/main.js
@@ -24,3 +24,17 @@ const swiper = new Swiper('.swiper', {
     },
     spaceBetween: 100,
   });
+
+  const showHideBTN = document.querySelector('#showHideBTN')
+  const showHideWrapp = document.querySelector('#showHideWrapp')
+
+
+
+  showHideBTN.addEventListener('click',()=>{
+    if (!showHideWrapp.classList.contains('display-none')) {
+      showHideBTN.innerHTML="Показать еще"
+    } else {
+      showHideBTN.innerHTML="Скрыть"
+    }
+    showHideWrapp.classList.toggle('display-none')
+  })
